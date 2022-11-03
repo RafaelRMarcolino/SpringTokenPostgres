ace3a2581c719c6057f9ce9d21eea296fc41a204
Rafel <ralfrr55@gmail.com>
2022-11-03 13:27:24 -0300

    spring securiry desbloqueio de validação

--- token-spring/src/main/java/com/token/token/spring/TokenSpringApplication.java
+++ token-spring/src/main/java/com/token/token/spring/TokenSpringApplication.java
@@ -2,8 +2,10 @@ package com.token.token.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//excluide permite utilizar o espring security sem pedir as validações de usuario e senha
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TokenSpringApplication {
 
 	public static void main(String[] args) {
