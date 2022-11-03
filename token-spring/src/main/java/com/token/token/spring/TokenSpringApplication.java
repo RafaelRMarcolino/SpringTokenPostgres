6518895603a3079e0747a9425f9bfe2d05eb4530
Rafel <ralfrr55@gmail.com>
2022-11-03 14:21:15 -0300

    validação de senha

--- token-spring/src/main/java/com/token/token/spring/TokenSpringApplication.java
+++ token-spring/src/main/java/com/token/token/spring/TokenSpringApplication.java
@@ -3,13 +3,25 @@ package com.token.token.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //excluide permite utilizar o espring security sem pedir as validações de usuario e senha
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TokenSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TokenSpringApplication.class, args);
 	}
+
+	// ben para ser um componente da aplicação
+	@Bean
+	public PasswordEncoder getPasswordEncolder(){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
+
 
 }
--- token-spring/src/main/java/com/token/token/spring/controller/UsuariosController.java
+++ token-spring/src/main/java/com/token/token/spring/controller/UsuariosController.java
@@ -2,11 +2,13 @@ package com.token.token.spring.controller;
 
 import com.token.token.spring.model.UsuarioModel;
 import com.token.token.spring.repository.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -14,8 +16,12 @@ public class UsuariosController {
 
     private final UsuarioRepository repository;
 
-    public UsuariosController(UsuarioRepository repository){
+    // adicionando o decoder
+    private final PasswordEncoder encoder;
+
+    public UsuariosController(UsuarioRepository repository, PasswordEncoder encoder){
         this.repository = repository;
+        this.encoder = encoder;
     }
 
     @GetMapping("/listarTodos")
@@ -26,7 +32,31 @@ public class UsuariosController {
 
     @PostMapping("/listarTodos")
     public ResponseEntity<UsuarioModel> insertUsuarios(@RequestBody UsuarioModel usuarioModel){
+       usuarioModel.setPassword(encoder.encode(usuarioModel.getPassword()));
         return ResponseEntity.ok(repository.save(usuarioModel));
+
+    }
+
+
+    //Rest para validar a senha
+    @GetMapping("/validarSenha")
+    public ResponseEntity<Boolean> validarSenha(@RequestParam String login,
+                                                @RequestParam String password){
+        // consulta pelo login
+        Optional<UsuarioModel> optUsuario = repository.findByLogin(login);
+        if(optUsuario.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+
+        UsuarioModel usuario = optUsuario.get();
+        // verificar se a senha que esta cadastrada e igual a senha digitada
+       boolean valid = encoder.matches(password, optUsuario.get().getPassword());
+
+        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(status).body(valid);
+
+        // testar na url http://localhost:8080/api/usuarios/validarSenha
 
     }
 
