ef00383cb72583abd2a0d567f8dfd7f55c3f0018
Rafel <ralfrr55@gmail.com>
2022-11-04 15:03:07 -0300

    jwt configure

---
+++ token-spring/src/main/java/com/token/token/spring/security/JWTConfiguracao.java
@@ -0,0 +1,71 @@
+package com.token.token.spring.security;
+
+import com.token.token.spring.service.DetalheUsuariosServicesImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class JWTConfiguracao  extends WebSecurityConfigurerAdapter {
+
+
+    private final DetalheUsuariosServicesImpl usuariosServices;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+    public JWTConfiguracao(DetalheUsuariosServicesImpl usuariosServices, PasswordEncoder passwordEncoder) {
+        this.usuariosServices = usuariosServices;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+        auth.userDetailsService(usuariosServices).passwordEncoder(passwordEncoder);
+
+    }
+
+    @Autowired
+    protected void configure(HttpSecurity http)throws Exception{
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAutenticarFilter(authenticationManager()))
+                .addFilter(new JWTValidarFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return  source;
+
+    }
+
+}
